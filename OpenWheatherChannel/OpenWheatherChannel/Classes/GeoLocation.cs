4ab9c0beeb5f6da51b002e6cef34bc3b6b9646e5
Vittorio_Vaselli <vitorio.vaselli@dev-ennova.com>
2015-12-01 20:04:04 +0100

    classes for get geocoordinate and weather

---
+++ OpenWheatherChannel/OpenWheatherChannel/Classes/GeoLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace OpenWheatherChannel.Classes
+{
+    class GeoLocation
+    {
+        public async Task<Geocoordinate> GetCurrentGeoCoordinates()
+        {
+            var geolocator = new Geolocator { DesiredAccuracy = PositionAccuracy.Default };
+            Geoposition position = await geolocator.GetGeopositionAsync();
+            return position.Coordinate;
+        }
+    }
+}
---
+++ OpenWheatherChannel/OpenWheatherChannel/Classes/WebServices.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Web.Http;
+
+namespace OpenWheatherChannel.Classes
+{
+    class WebServices
+    {
+        public string CURRENTWEATHERBYGEOCOORDINATES = "api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}";
+
+        public async Task<WeatherResponse> GetCurrentWheatherByGeoCoordinates(double lat, double lon)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, new Uri(String.Format(CURRENTWEATHERBYGEOCOORDINATES, lat, lon)));
+                var httpResponse = await new HttpClient().SendRequestAsync(request);
+                httpResponse.EnsureSuccessStatusCode();
+                var response = await httpResponse.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<WeatherResponse>(response) ;
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+                return null;
+            }
+
+        }
+
+    }
+}
